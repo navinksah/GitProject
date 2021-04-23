57509fd543b1bbcb52c506c8e364587bc8bce3ef
NAVIN KUMAR SAH <navinsah14@gmail.com>
2021-04-23 17:34:17 +0530

    Revert "commit2"
    This reverts commit 74fd9948f8fd4ec50952247b73c3029bc691b39d.

--- GitProject/Program.cs
+++ GitProject/Program.cs
@@ -1,25 +1,12 @@
 ﻿using System;
-<<<<<<< Updated upstream
 
-=======
-// First  create a project in console application 
-// then go to git and aa repository
-// after goig to reposoitory go to local and create
-// after that go to master in right bottom and make 1 more branch 
-// branch is beacause we didin't use main master for any update 
-// then go to view and git changes 
-// then make commit command 
-// after that go to master again and go to manage branch for merging the branch with master
-// after that i make changes in branch 1 and then to see the changes we go to
-// git changes and then go to program and right click and compare modified with unmodified
->>>>>>> Stashed changes
 namespace GitProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Branch  1 after editing once more");
+            Console.WriteLine("Hello World! Branch  1");
 
         }
     }
